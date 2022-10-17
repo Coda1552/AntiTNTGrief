91f26c8a702506cdf8721343aed590394537e5bb
Cody <cody.koelliker@outlook.com>
2022-10-17 16:11:45 -0600

    boom begone

--- src/main/java/coda/antitntgrief/BoomBegone.java
+++ src/main/java/coda/antitntgrief/BoomBegone.java
@@ -5,15 +5,14 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.ExplosionEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import java.util.List;
 
-@Mod(AntiTNTGrief.MOD_ID)
-public class AntiTNTGrief {
-    public static final String MOD_ID = "antitntgrief";
+@Mod(BoomBegone.MOD_ID)
+public class BoomBegone {
+    public static final String MOD_ID = "boombegone";
 
-    public AntiTNTGrief() {
+    public BoomBegone() {
         IEventBus forgeBus = MinecraftForge.EVENT_BUS;
 
         forgeBus.addListener(this::explode);
@@ -21,10 +20,7 @@ public class AntiTNTGrief {
 
     private void explode(ExplosionEvent.Detonate event) {
         List<BlockPos> blocks = event.getAffectedBlocks();
-
-        for (BlockPos pos : blocks) {
-            blocks.remove(pos);
-        }
+        blocks.clear();
     }
-
 }
+
